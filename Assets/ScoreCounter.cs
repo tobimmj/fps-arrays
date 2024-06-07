657068c8f30f1684aaef614a2d91dc248b622362
tobimjc <tobiasmajic@gmail.com>
2024-06-07 09:13:15 -0300

    Initial commit

---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+    [SerializeField] private Text txtScore;
+    
+    public int Score
+    {
+        get { return score; }
+        set
+        {
+            txtScore.text = value.ToString();
+            score = value;
+        }
+    }
+
+    private int score;
+
+    void Start()
+    {
+        txtScore.text = "0";
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Food"))
+            return;
+        Alimento food = collision.gameObject.GetComponent<Alimento>();
+        Score += food.saturation;
+        Destroy(food.gameObject);
+    }
+}
---
+++ Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+
+    [SerializeField] private float interval;
+    [SerializeField] private List<GameObject> food;
+
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnFood), interval, 1);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void SpawnFood()
+    {
+        Instantiate(GetRandomFood(), GetRandomPosition(), Quaternion.identity);
+    }
+
+    GameObject GetRandomFood()
+    {
+        return food[Random.Range(0, food.Count)];
+    }
+
+    Vector3 GetRandomPosition()
+    {
+        float x = Random.Range(-transform.localScale.x/2.0f,
+            transform.localScale.x/2.0f);
+        float z = Random.Range(-transform.localScale.z / 2.0f,
+            transform.localScale.z / 2.0f);
+
+        return new Vector3(x, transform.position.y, z);
+    }
+}
